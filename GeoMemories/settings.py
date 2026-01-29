b3184c2a6d4a7847b27e1ec8afa2da83908b702d
Neomeniam <dnmfuture@gmail.com>
2026-01-29 23:53:35 +0800

    Enable Pagination in Django

--- GeoMemories/settings.py
+++ GeoMemories/settings.py
@@ -74,6 +74,9 @@ REST_FRAMEWORK = {
     'DEFAULT_PERMISSION_CLASSES': (
         'rest_framework.permissions.IsAuthenticated',
     ),
+    # --- ADD THESE LINES TO FIX THE CRASH ---
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
+    'PAGE_SIZE': 10,
 }
 
 # FIX: Extend Token Lifetime to prevent constant 401 errors during Dev
